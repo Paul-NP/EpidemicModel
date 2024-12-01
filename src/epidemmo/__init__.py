14fd74595a0a3839f414efffa88f4d165d95dc03
Paul-NP <ppodzolkoff@gmail.com>
2024-12-01 16:04:49 +0500

    rename version to 0.1.4

--- src/epidemmo/__init__.py
+++ src/epidemmo/__init__.py
@@ -3,6 +3,6 @@ from .standard import Standard
 from .model_io import ModelIO
 from .fit_model import ModelFitter
 
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 print(f'epidemmo version: {__version__}')
