745785328e3f99bd8623c42cd81bd6e86e549c49
Paul-NP <ppodzolkoff@gmail.com>
2024-12-02 16:19:32 +0500

    remove extra dir from distribution, version 0.1.5

--- src/epidemmo/__init__.py
+++ src/epidemmo/__init__.py
@@ -3,6 +3,6 @@ from .standard import Standard
 from .model_io import ModelIO
 from .fit_model import ModelFitter
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 
 print(f'epidemmo version: {__version__}')
