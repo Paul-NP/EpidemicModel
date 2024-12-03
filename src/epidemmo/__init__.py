9592cd19d4d7c8ed6a203d2bb3eace4ecbbad991
Paul-NP <ppodzolkoff@gmail.com>
2024-12-03 20:14:09 +0500

    version 0.1.6

--- src/epidemmo/__init__.py
+++ src/epidemmo/__init__.py
@@ -3,6 +3,6 @@ from .standard import Standard
 from .model_io import ModelIO
 from .fit_model import ModelFitter
 
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 
 print(f'epidemmo version: {__version__}')
